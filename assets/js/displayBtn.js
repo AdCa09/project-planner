958df6a540ad83460c42efab80a72de90dfb6818
AdCa09 <caufriezadrien0@gmail.com>
2024-05-07 16:08:25 +0200

    add task btn

---
+++ assets/js/displayBtn.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.getElementById('addTask');
+    let form = document.querySelector('form');
+    let display = false;
+
+    btn.addEventListener('click', function(){
+        if(display){
+            form.style.visibility = 'visible';
+            form.style.opacity = '1'; 
+            form.style.height = 'auto'; 
+            form.style.padding = '20px'; 
+            display = false;
+        } else {
+            form.style.visibility = 'hidden';
+            form.style.opacity = '0'; 
+            form.style.height = '0'; 
+            form.style.padding = '0';
+            display = true;
+        }
+    });
+});
