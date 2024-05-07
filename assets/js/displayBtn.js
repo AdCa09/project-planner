4a54ee8078927efb55203c01da868ba1a53c6123
AdCa09 <caufriezadrien0@gmail.com>
2024-05-07 16:38:17 +0200

    modification pour btn close

--- assets/js/displayBtn.js
+++ assets/js/displayBtn.js
@@ -2,21 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
     let btn = document.getElementById('addTask');
     let form = document.querySelector('form');
     let display = false;
-
+    let close = document.getElementById('closeButton');
+    
     btn.addEventListener('click', function(){
-        if(display){
+        if (!display) {
             form.style.visibility = 'visible';
             form.style.opacity = '1'; 
             form.style.height = 'auto'; 
             form.style.padding = '20px'; 
-            display = false;
-        } else {
-            form.style.visibility = 'hidden';
-            form.style.opacity = '0'; 
-            form.style.height = 'auto'; 
-            form.style.padding = '0';
-
             display = true;
         }
+    });
+    
+    close.addEventListener('click', function(){
+        form.style.visibility = 'hidden';
+        form.style.opacity = '0'; 
+        form.style.height = 'auto'; 
+        form.style.padding = '0';
+        display = false;
     });
 });
